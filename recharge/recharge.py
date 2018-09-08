53856d165c4aaa3e0f24faea5c3623eccc2226bd
Undefined <emmanuel.utrum@pm.me>
2018-09-08 22:21:57 +0000

    deleted empty lines

--- recharge/recharge.py
+++ recharge/recharge.py
@@ -152,12 +152,10 @@ def main():
         if coin == 'DEFAULT':
             continue
         print(coin)
-
         # define rpc parameters
         rpc_params = get_coin_rpc(coin, config)
         rpcurl = rpc_params[0]
         rpcauth = rpc_params[1]
-
         # define utxo size to filter by
         utxo_size = config[coin]['utxo_size']
         # ask how many utxo's
